1ebb945fb0eb52479b47bc996634739dc2cc785b
NasFahmi <75nasrulfahmi@gmail.com>
2024-04-14 21:29:24 +0700

    fix style edit

--- resources/views/pages/admin/product/edit.blade.php
+++ resources/views/pages/admin/product/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container  px-6 pb-6 mx-auto">
         <h1 class="text-2xl my-6 font-semibold text-gray-700 ">Edit Product</h1>
-        <div class="bg-white flex justify-center items-center px-8 py-8 shadow-lg rounded-3xl">
+        <div class="bg-white  px-8 py-8 shadow-lg rounded-3xl">
 
             <form action="{{ route('products.update', $data->id) }}" method="POST" enctype="multipart/form-data">
                 @method('patch')

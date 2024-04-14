2069ee4c52651c567dba6300f6998d2bdac7b835
NasFahmi <75nasrulfahmi@gmail.com>
2024-04-14 22:22:09 +0700

    fixing crud beban kewajiban

--- resources/views/pages/beban-kewajiban/index.blade.php
+++ resources/views/pages/beban-kewajiban/index.blade.php
@@ -144,7 +144,7 @@
                                             <li>
                                                 @if (auth()->check() && auth()->user()->hasRole('superadmin'))
                                                     <a href="{{ route('beban-kewajibans.edit', $items->id) }}"
-                                                        class="block px-4 py-2 hover:bg-gray-100 bg-green-50">
+                                                        class="block px-4 py-2 hover:bg-green-100 bg-green-50">
                                                         <div class="flex justify-start items-center gap-2">
                                                             <div class="w-4 h-4 ">
                                                                 <svg viewBox="0 0 24 24" fill="none"
@@ -169,25 +169,33 @@
                                             </li>
 
                                             <li>
-                                                <a href="{{ route('beban-kewajibans.destroy', $items->id) }}"
-                                                    class="block px-4 py-2 hover:bg-sky-100 bg-sky-50">
-                                                    <div class="flex justify-start items-center gap-2">
-                                                        <div class="w-4 h-4 ">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16"
-                                                                height="16" fill="currentColor" class="bi bi-trash"
-                                                                viewBox="0 0 16 16">
-                                                                <path
-                                                                    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"
-                                                                    fill="red" />
-                                                                <path
-                                                                    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"
-                                                                    fill="red" />
-                                                            </svg>
-                                                        </div>
-                                                        <span class="font-semibold text-red-400 ">Hapus</span>
-                                                    </div>
-                                                </a>
+                                                @if (auth()->check() && auth()->user()->hasRole('superadmin'))
+                                                    <form action="{{ route('beban-kewajibans.destroy', $items->id) }}"
+                                                        method="POST">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit"
+                                                            class="block px-4 py-2 w-full hover:bg-red-100 bg-red-50">
+                                                            <div class="flex justify-start items-center gap-2">
+                                                                <div class="w-4 h-4">
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16"
+                                                                        height="16" fill="currentColor"
+                                                                        class="bi bi-trash" viewBox="0 0 16 16">
+                                                                        <path
+                                                                            d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"
+                                                                            fill="red" />
+                                                                        <path
+                                                                            d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"
+                                                                            fill="red" />
+                                                                    </svg>
+                                                                </div>
+                                                                <span class="font-semibold text-red-400">Hapus</span>
+                                                            </div>
+                                                        </button>
+                                                    </form>
+                                                @endif
                                             </li>
+
 
                                         </ul>
                                     </div>

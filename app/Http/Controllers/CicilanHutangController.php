d9067d95eaff2cfd62dff547cb6a943ca82c01b0
NasFahmi <75nasrulfahmi@gmail.com>
2024-06-01 21:11:45 +0700

    migrate hutang cicilan

---
+++ app/Http/Controllers/CicilanHutangController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CicilanHutang;
+use App\Http\Requests\StoreCicilanHutangRequest;
+use App\Http\Requests\UpdateCicilanHutangRequest;
+
+class CicilanHutangController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCicilanHutangRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CicilanHutang $cicilanHutang)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CicilanHutang $cicilanHutang)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCicilanHutangRequest $request, CicilanHutang $cicilanHutang)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CicilanHutang $cicilanHutang)
+    {
+        //
+    }
+}
--- app/Models/CicilanHutang.php
+++ app/Models/CicilanHutang.php
@@ -4,15 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Hutang extends Model
+class CicilanHutang extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
     protected $fillable = [
-        'nama',
-        'catatan',
+        'hutangId',
+        'nominal',
+        'tanggal',
         'status',
-        'tanggal_lunas',
     ];
 }

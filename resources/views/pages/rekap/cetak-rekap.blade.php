b62a8d0f43b7dac49ea0bfab3752a19d0093d511
Pafaz <paundra.afif@gmail.com>
2024-12-24 18:22:14 +0700

    fix: fixing cetak rekap

--- resources/views/pages/rekap/cetak-rekap.blade.php
+++ resources/views/pages/rekap/cetak-rekap.blade.php
@@ -1,8 +1,56 @@
-@extends('layout.admin_pages')
-@section('title', 'Cetak Rekapan')
-@section('content')
+
+<!DOCTYPE html>
+<html :class="{ 'theme-dark': light }" data-theme="light" x-data="data()" lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Cetak Rekapan</title>
+    <style>
+        .cover-image {
+            object-fit: cover;
+            width: 100%;
+            height: 100%;
+        }
+    </style>
+
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo.png') }}" type="image/x-icon">
+    @include('partials.link')
+    @extends('partials.head')
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <link href="https://unpkg.com/filepond@^4/dist/filepond.css" rel="stylesheet" />
+    <link href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css"
+        rel="stylesheet" />
+
+    <style>
+        /* Add this in your CSS or within a style tag in your HTML */
+        .costumscroll::-webkit-scrollbar {
+            width: 8px;
+            /* Set the width of the scrollbar */
+        }
+
+        .costumscroll::-webkit-scrollbar-thumb {
+            background-color: #cbd5e1;
+            /* Set the color of the thumb (the draggable part) */
+            border-radius: 8px;
+            /* Set the border-radius of the thumb */
+        }
+
+        .costumscroll::-webkit-scrollbar-track {
+            background-color: #f1f1f1;
+            /* Set the color of the track (the non-draggable part) */
+        }
+    </style>
+</head>
+
+<body>
+    
+ 
+
+
+
 <div class="container mx-auto my-8 p-8 bg-white shadow-lg">
-    <p class="text-center text-2xl font-bold">Laporan Rekapan Keuangany</p>
+    <p class="text-center text-2xl font-bold">Laporan Rekapan Keuangan</p>
     
     <!-- Kategori Uang Masuk -->
     <h2 class="text-xl font-semibold mt-6 mb-4">Uang Masuk</h2>
@@ -71,4 +119,7 @@
 <script type="text/javascript">
     window.print();
 </script>
-@endsection
+@extends('partials.link')
+</body>
+
+</html>

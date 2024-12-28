36718f1a658bf3e03839e14b09154f13bcc07323
Nasrul Fahmi <75nasrulfahmi@gmail.com>
2024-12-28 18:43:08 +0700

    fix : fixing date in edit

--- resources/views/pages/beban-kewajiban/edit.blade.php
+++ resources/views/pages/beban-kewajiban/edit.blade.php
@@ -2,34 +2,39 @@
 @section('title', 'Admin Beban dan Kewajiban')
 @section('content')
     <div class="container px-6 pb-6 mx-auto">
-        <h1 class="text-2xl my-6 font-semibold text-gray-700">Edit Beban & Kewajiban</h1>
+        <h1 class="my-6 text-2xl font-semibold text-gray-700">Edit Beban & Kewajiban</h1>
         {{-- {{$data}} --}}
-        <div class="bg-white px-8 py-8 shadow-lg rounded-3xl">
+        <div class="px-8 py-8 bg-white shadow-lg rounded-3xl">
             <form action="{{ route('beban-kewajibans.update', $data->id) }}" method="post">
                 @method('PATCH')
                 @csrf
-                <div class="grid grid-cols-1 lg:grid-cols-2 gap-10">
+                <div class="grid grid-cols-1 gap-10 lg:grid-cols-2">
                     <div class="left">
                         <div class="max-w-lg">
                             <div class="flex flex-col gap-3">
                                 <div class="w-full">
                                     <label for="jenis" class="block mb-2 text-sm font-medium text-gray-700">Jenis</label>
-                                    <select name="jenis" class="bg-gray-50 border max-w-4xl border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                                    <select name="jenis"
+                                        class="bg-gray-50 border max-w-4xl border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                                         <option value="" disabled selected>Pilih Jenis</option>
-                                        <option value="beban" {{ old('jenis', $data->jenis) == 'beban' ? 'selected' : '' }}>Beban</option>
-                                        <option value="kewajiban" {{ old('jenis', $data->jenis) == 'kewajiban' ? 'selected' : '' }}>Kewajiban</option>
+                                        <option value="beban"
+                                            {{ old('jenis', $data->jenis) == 'beban' ? 'selected' : '' }}>Beban</option>
+                                        <option value="kewajiban"
+                                            {{ old('jenis', $data->jenis) == 'kewajiban' ? 'selected' : '' }}>Kewajiban
+                                        </option>
                                     </select>
                                     @error('jenis')
-                                        <small class="error text-red-500">{{ $message }}</small>
+                                        <small class="text-red-500 error">{{ $message }}</small>
                                     @enderror
                                 </div>
 
                                 <div class="w-full mt-4">
                                     <label for="nama" class="block mb-2 text-sm font-medium text-gray-700">Nama</label>
-                                    <input type="text" name="nama" value="{{ old('nama', $data->nama) }}" placeholder="Nama"
-                                           class="bg-gray-50 border max-w-4xl border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+                                    <input type="text" name="nama" value="{{ old('nama', $data->nama) }}"
+                                        placeholder="Nama"
+                                        class="bg-gray-50 border max-w-4xl border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
                                     @error('nama')
-                                        <small class="error text-red-500">{{ $message }}</small>
+                                        <small class="text-red-500 error">{{ $message }}</small>
                                     @enderror
                                 </div>
                             </div>
@@ -40,10 +45,10 @@
                         <div class="w-full">
                             <label for="nominal" class="block mb-2 text-sm font-medium text-gray-700">Nominal</label>
                             <input type="number" name="nominal" value="{{ old('nominal', $data->nominal) }}" min="0"
-                                   placeholder="Nominal" oninput="this.value = this.value.replace(/^0+(?!$)/, '')"
-                                   class="bg-gray-50 border max-w-4xl border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+                                placeholder="Nominal" oninput="this.value = this.value.replace(/^0+(?!$)/, '')"
+                                class="bg-gray-50 border max-w-4xl border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
                             @error('nominal')
-                                <small class="error text-red-500">{{ $message }}</small>
+                                <small class="text-red-500 error">{{ $message }}</small>
                             @enderror
                         </div>
 
@@ -57,7 +62,8 @@
                                             d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                                     </svg>
                                 </div>
-                                <input datepicker type="text" name="tanggal"value="{{ old('tanggal', \Carbon\Carbon::parse($data->tanggal)->format('m/d/Y')) }}" 
+                                <input datepicker datepicker-format="yyyy-mm-dd" type="text"
+                                    name="tanggal"value="{{ old('tanggal', \Carbon\Carbon::parse($data->tanggal)->format('m/d/Y')) }}"
                                     class="bg-gray-50 border max-w-4xl border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     placeholder="Select date">
                             </div>
@@ -66,13 +72,13 @@
                             @enderror
                         </div>
 
-                        
+
                     </div>
                 </div>
 
-                <div class="flex justify-center items-center mt-8">
+                <div class="flex items-center justify-center mt-8">
                     <button type="submit"
-                            class="bg-green-400 text-white px-4 py-2 w-full lg:w-fit rounded-lg hover:bg-green-500 transition duration-300">Submit</button>
+                        class="w-full px-4 py-2 text-white transition duration-300 bg-green-400 rounded-lg lg:w-fit hover:bg-green-500">Submit</button>
                 </div>
             </form>
         </div>

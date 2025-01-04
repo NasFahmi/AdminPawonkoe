aa97a87c124cfdcdc424937a33bce5d3faabaa36
Nasrul Fahmi <75nasrulfahmi@gmail.com>
2025-01-04 16:51:35 +0700

    fix: piutang

--- app/Http/Controllers/PiutangController.php
+++ app/Http/Controllers/PiutangController.php
@@ -53,7 +53,7 @@ class PiutangController extends Controller
         // Validasi input data
         $validatedData = $request->validate([
             'nama_toko' => 'required|string',
-            'sewa_titip' => 'required|numeric|min:1|regex:/^[1-9][0-9]*$/',
+            'sewa_titip' => 'required|numeric|min:0|regex:/^[1-9][0-9]*$/',
             'tanggal_disetorkan' => 'required|',
             'catatan' => 'nullable|string',
             'product.*.product' => 'required|string',
